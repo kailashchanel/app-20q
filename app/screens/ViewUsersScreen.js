1829b0679c4890cb01b093b639f559d6873a512d
Kailash E <kailash@kumonofmansfield.com>
2022-07-18 13:19:52 -0400

    Code formatting

--- app/screens/ViewUsersScreen.js
+++ app/screens/ViewUsersScreen.js
@@ -1,14 +1,14 @@
-import React, {useEffect} from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import React, { useEffect } from "react";
+import { FlatList, StyleSheet } from "react-native";
 
 import ActivityIndicator from "../components/ActivityIndicator";
-import Button from '../components/Button';
-import Card from '../components/Card';
-import colors from '../config/colors';
-import compareApi from '../api/compare';
+import Button from "../components/Button";
+import Card from "../components/Card";
+import colors from "../config/colors";
+import compareApi from "../api/compare";
 import routes from "../navigation/routes";
-import Screen from '../components/Screen';
-import Text from '../components/Text';
+import Screen from "../components/Screen";
+import Text from "../components/Text";
 import useApi from "../hooks/useApi";
 
 function ViewUsersScreen({ navigation }) {
@@ -17,15 +17,15 @@ function ViewUsersScreen({ navigation }) {
     data: comparisons,
     loading,
     error,
-    preConditionFailed
+    preConditionFailed,
   } = useApi(compareApi.getAllComparisons);
 
   // useEffect(() => {
   //   loadComparisons();
-  // }, []);  
+  // }, []);
 
   useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       loadComparisons();
     });
 
@@ -44,30 +44,37 @@ function ViewUsersScreen({ navigation }) {
         )}
         {preConditionFailed && (
           <>
-            <Text style={styles.text}>Take the survey to see your recommendations!</Text>
-            <Button title="Take me to the survey!" onPress={() => navigation.navigate(routes.SURVEY)} />
-            <Button color="secondary" title="Press me when you're done!" onPress={loadComparisons} />
+            <Text style={styles.text}>
+              Take the survey to see your recommendations!
+            </Text>
+            <Button
+              title="Take me to the survey!"
+              onPress={() => navigation.navigate(routes.SURVEY)}
+            />
+            <Button
+              color="secondary"
+              title="Press me when you're done!"
+              onPress={loadComparisons}
+            />
           </>
         )}
-        {comparisons && 
-        <>
-        <Text style={styles.text}>
-          Recommended For You
-        </Text>        
-        <FlatList
-          data={comparisons.sort((a, b) => a.percent < b.percent)}
-          keyExtractor={(data) => data.userId.toString()}
-          renderItem={({ item }) => (
-            <Card
-                name={item.name}
-                percent={item.percent}
-                onPress={() => navigation.navigate(routes.DETAILS, item)}
+        {comparisons && (
+          <>
+            <Text style={styles.text}>Recommended For You</Text>
+            <FlatList
+              data={comparisons.sort((a, b) => a.percent < b.percent)}
+              keyExtractor={(data) => data.userId.toString()}
+              renderItem={({ item }) => (
+                <Card
+                  name={item.name}
+                  percent={item.percent}
+                  onPress={() => navigation.navigate(routes.DETAILS, item)}
+                />
+              )}
+              showsVerticalScrollIndicator={false}
             />
-          )}
-          showsVerticalScrollIndicator={false}
-        />
-        </>
-        }
+          </>
+        )}
       </Screen>
     </>
   );
@@ -81,7 +88,7 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 25,
     marginBottom: 20,
-  }
+  },
 });
 
 export default ViewUsersScreen;
